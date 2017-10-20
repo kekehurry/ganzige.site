ef1db2a94341c2ea0fed74f156619bf38d25f4ad
kekehurry <837082742@qq.com>
2017-10-20 12:50:52 +0800

    ganzige

--- home/views.py
+++ home/views.py
@@ -10,7 +10,7 @@ from .models import Message_board, Background
 def home(request):
     blogs = Blog.objects.order_by('-pub_time')[:4]
     recent_photos = Photo.objects.order_by('-pub_time')[:8]
-    background = Background.objects.order_by('-pub_time')[0]
+    background = Background.objects.order_by('-pub_time')[:1]
     message = '欢迎各位小伙伴前来吐槽~'
     if request.POST:
         message = '留言已提交，谢谢您的关注！'
