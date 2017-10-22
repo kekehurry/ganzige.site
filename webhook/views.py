487dbc740662493b99aad15710af77b3a3068e1e
kekehurry <kekehurry@hotmail.com>
2017-10-23 00:12:32 +0800

    view

--- webhook/views.py
+++ webhook/views.py
@@ -8,8 +8,9 @@ from django.views.decorators.csrf import csrf_exempt
 @csrf_exempt
 def webhook(request):
     json = request.POST
-    if json:
+    if json and json["commits"][1]["committer"]["name"] == 'kekehurry':
         os.system('chmod a+x /usr/www/webhook.sh')
         status = os.popen('/usr/www/webhook.sh')
         return HttpResponse('post_recieved')
-    return HttpResponse('Done')
+    else:
+        return HttpResponse('unknow_post')
