dbfbb7a344a95c92810dfd932c50141a317d3f89
kekehurry <kekehurry@hotmail.com>
2017-10-23 00:24:59 +0800

    json

--- webhook/views.py
+++ webhook/views.py
@@ -9,6 +9,11 @@ from django.views.decorators.csrf import csrf_exempt
 def webhook(request):
     if request.method == 'POST':
         json = request.POST
+        try:
+            with open('/usr/www/json.txt') as f:
+                f.write(json)
+        except Exception as e:
+            pass
         os.system('chmod a+x /usr/www/webhook.sh')
         status = os.popen('/usr/www/webhook.sh')
         return HttpResponse('post_recieved')
