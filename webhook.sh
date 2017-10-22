2588019ef29a156ff11ed3a5c66adcd8c759d518
kekehurry <kekehurry@hotmail.com>
2017-10-22 23:43:40 +0800

    c

--- webhook.sh
+++ webhook.sh
@@ -4,6 +4,5 @@ git fetch --all
 git reset --hard origin/master
 git pull
 pid=`lsof -i:8001| awk 'NR==2{print $2}'`　
-echo "pid:$pid"
-kill -9 `$pid`　
+kill -9 `eval $pid`　
 nohup uwsgi --ini /usr/www/uwsgi.ini
