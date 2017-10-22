dfadc4c21d4c0f9a50b34447c1ed86e8f641c643
kekehurry <kekehurry@hotmail.com>
2017-10-23 00:31:46 +0800

    x

--- webhook/views.py
+++ webhook/views.py
@@ -1,5 +1,6 @@
 
 import os
+import json
 # Create your views here.
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
@@ -8,10 +9,10 @@ from django.views.decorators.csrf import csrf_exempt
 @csrf_exempt
 def webhook(request):
     if request.method == 'POST':
-        json = request.POST
+        json = json.loads(request.body)
         try:
             with open('/usr/www/json.txt') as f:
-                f.write(json)
+                f.writelines(json)
         except Exception as e:
             pass
         os.system('chmod a+x /usr/www/webhook.sh')
