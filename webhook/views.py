fa6a3ca561cd8d538c142f5124a33960537cfaae
kekehurry <kekehurry@hotmail.com>
2017-10-22 22:38:06 +0800

    g

--- webhook/views.py
+++ webhook/views.py
@@ -8,7 +8,6 @@ from django.views.decorators.csrf import csrf_exempt
 @csrf_exempt
 def webhook(request):
     json = request.POST
-    if json:
-        os.system('chmod a+x /usr/www/webhook.sh')
-        status = os.popen('/usr/www/webhook.sh')
-        return HttpResponse('Done')
+    os.system('chmod a+x /usr/www/webhook.sh')
+    status = os.popen('/usr/www/webhook.sh')
+    return HttpResponse('Done')
