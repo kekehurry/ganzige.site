cc82e0cb79f0f412bc5b88151bed885a8a420a24
kekehurry <kekehurry@hotmail.com>
2017-10-22 17:32:14 +0800

    hello

--- webhook/views.py
+++ webhook/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 import os
 # Create your views here.
+from django.http import HttpResponse
 
 
 def webhook(request):
     json = request.POST
     if json:
-        if json['commits'][0]['committer']['name'] == 'kekehurry':
-            os.system('chomd a+x /usr/www/webhook/webhook.sh')
-            os.popen('webhook.sh')
-    return
+        os.system('chomd a+x /usr/www/webhook/webhook.sh')
+        os.popen('webhook.sh')
+    return HttpResponse("Hello, world. You're at the polls index.")
