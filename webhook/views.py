6160209e78814e490305c69cd0bea7801b1bf280
kekehurry <kekehurry@hotmail.com>
2017-10-22 17:40:09 +0800

    webhook

--- webhook/views.py
+++ webhook/views.py
@@ -7,6 +7,6 @@ from django.http import HttpResponse
 def webhook(request):
     json = request.POST
     if json:
-        os.system('chomd a+x /usr/www/webhook/webhook.sh')
-        os.popen('webhook.sh')
-    return HttpResponse("Hello, world. You're at the polls index.")
+        os.poen('chomd a+x /usr/www/webhook/webhook.sh')
+        status = os.popen('webhook.sh')
+    return HttpResponse(status)
