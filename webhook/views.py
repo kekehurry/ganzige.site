7b5aaf12e7b8337d7fa033bbf20e5fb5e28816cc
kekehurry <kekehurry@hotmail.com>
2017-10-22 18:56:57 +0800

    kekehurry

--- webhook/views.py
+++ webhook/views.py
@@ -7,7 +7,7 @@ from django.http import HttpResponse
 def webhook(request):
     json = request.POST
     status = ''
-    if json and json['commits'][0]['committer']['email']:
+    if json and json['commits'][0]['committer']['email']=='kekehurry':
         os.system('chmod a+x /usr/www/webhook/webhook.sh')
         status = os.popen('/usr/www/webhook/webhook.sh')
     return HttpResponse(status)
