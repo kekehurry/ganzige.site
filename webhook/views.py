8d330efb710a8b35f2d33010e1a290729ac7691a
kekehurry <kekehurry@hotmail.com>
2017-10-22 17:22:50 +0800

    check

--- webhook/views.py
+++ webhook/views.py
@@ -5,7 +5,7 @@ import os
 
 def webhook(request):
     json = request.POST
-    if json:
+    if json['commits'][0]['committer']['name'] == 'kekehurry':
         os.system('chomd a+x /usr/www/webhook/webhook.sh')
         os.popen('webhook.sh')
-    return HttpResponse("Done!")
+    return
