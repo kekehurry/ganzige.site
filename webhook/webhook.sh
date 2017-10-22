c1c1edb999326d7957529c8296776cee9b8e4890
kekehurry <kekehurry@hotmail.com>
2017-10-22 16:37:57 +0800

    change webhook.sh

--- webhook/webhook.sh
+++ webhook/webhook.sh
@@ -1,3 +1,7 @@
 #!/bin/bash -l
-GIT_REPO=https://github.com/kekehurry/ganzige.site.git
-git clone $GIT_REPO $TMP_GIT_CLONE　
+git pull origin master　
+pid=`lsof -i:8000| awk 'NR==2{print $2}'`　
+echo "pid:$pid"　
+kill -9 $pid　
+lsof -i:8001
+nohup uwsgi --ini /usr/www/uwsgi.ini
