648334a99062f2b83ebadf24cbdaa5a9242b03fa
kekehurry <kekehurry@hotmail.com>
2017-10-23 00:00:43 +0800

    webhook update

--- webhook.sh
+++ webhook.sh
@@ -4,4 +4,4 @@ git fetch --all
 git reset --hard origin/master
 git pull
 ps -ef|grep uwsgi|grep -v grep|cut -c 9-15|xargs kill -s 9
-nohup uwsgi --ini /usr/www/uwsgi.ini
+nohup uwsgi --ini /usr/www/uwsgi.ini &
