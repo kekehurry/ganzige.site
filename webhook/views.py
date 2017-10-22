70014bb1024c489943739786ffe1484608484a33
kekehurry <kekehurry@hotmail.com>
2017-10-22 21:57:16 +0800

    commit

--- webhook/views.py
+++ webhook/views.py
@@ -1,7 +1,7 @@
 
 import os
 # Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.views.decorators.csrf import csrf_exempt
 
 
@@ -11,4 +11,8 @@ def webhook(request):
     if json:
         os.system('chmod a+x /usr/www/webhook.sh')
         status = os.popen('/usr/www/webhook.sh')
-        return HttpResponse(status.readlines()[0])
+        with open('/usr/www/json.log') as f:
+            f.writelines(json)
+        return HttpResponse('post_recieved')
+    else:
+        return Http404
