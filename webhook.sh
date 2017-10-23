e9bdea394b9020a204ba40cf1fdc44f83a8e7a0a
kekehurry <kekehurry@hotmail.com>
2017-10-23 11:03:20 +0800

    log

--- webhook.sh
+++ webhook.sh
@@ -6,5 +6,5 @@ git pull
 python manage.py makemigrations
 python manage.py migrate
 ps -ef|grep uwsgi|grep -v grep|cut -c 9-15|xargs kill -s 9
-nohup uwsgi --ini /usr/www/uwsgi.ini &
+nohup uwsgi --ini /usr/www/uwsgi.ini > log.txt 2>&1 &
 lsof -i :8001
