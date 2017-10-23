96c88e888ed70c3b3a0adab83fb63bcc93eef66d
kekehurry <kekehurry@hotmail.com>
2017-10-23 10:29:56 +0800

    Revert "settings"
    This reverts commit 16c4914b53f21c025e46524c85c6e0fe29fa87b5.

--- webhook.sh
+++ webhook.sh
@@ -3,7 +3,6 @@ cd /usr/www
 git fetch --all
 git reset --hard origin/master
 git pull
-python manage.py makemigrations
-python manage.py migrate
 ps -ef|grep uwsgi|grep -v grep|cut -c 9-15|xargs kill -s 9
 nohup uwsgi --ini /usr/www/uwsgi.ini &
+lsof -i :8001
