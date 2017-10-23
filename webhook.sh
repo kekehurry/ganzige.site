33a2dbacf5d216ad21064896f5193b4740bd03a4
kekehurry <kekehurry@hotmail.com>
2017-10-23 21:18:57 +0800

    cc

--- webhook.sh
+++ webhook.sh
@@ -5,4 +5,5 @@ python manage.py makemigrations
 python manage.py migrate
 ps -ef|grep uwsgi|grep -v grep|cut -c 9-15|xargs kill -s 9
 nohup uwsgi --ini /usr/www/uwsgi.ini > /dev/null 2>&1 &
+lsof -i :8001
 python mail.py
