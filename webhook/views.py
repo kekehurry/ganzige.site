f0ecabdd2709d72bcab7bfde8554c8cf8e55fd8b
kekehurry <kekehurry@hotmail.com>
2017-10-23 12:01:57 +0800

    ls

---
+++ webhook/views.py
@@ -0,0 +1,18 @@
+
+import os
+import json
+# Create your views here.
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+
+
+@csrf_exempt
+def webhook(request):
+    if request.method == 'POST':
+        data = json.loads(request.body.decode('utf-8'))
+        if data["commits"][0]["committer"]["name"] == "kekehurry":
+            os.system('chmod a+x /usr/www/webhook.sh')
+            status = os.popen('nohup /usr/www/webhook.sh > log.txt 2>&1 &')
+        return HttpResponse('post_recieved')
+    else:
+        return HttpResponse('unknow_post')
