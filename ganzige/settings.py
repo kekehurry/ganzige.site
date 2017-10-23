f2cc29db69e3f864570c2f1c8ab2cd55049e1374
kekehurry <kekehurry@hotmail.com>
2017-10-23 11:14:18 +0800

    test

--- ganzige/settings.py
+++ ganzige/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'j!o@k8hjlc0-(fapg8zomi%++8s^5^wrfbf$-3rh3t5i8ymj(5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
