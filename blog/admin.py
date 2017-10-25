cbed89c2f68982660e4c350925c076fc7c25fa7d
kekehurry <kekehurry@hotmail.com>
2017-10-25 08:41:35 +0800

    update database

--- blog/admin.py
+++ blog/admin.py
@@ -1,16 +1,27 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Blog, Detail
+from .models import Author, Blog, Detail, Tag
 
 
 class DetailInline(admin.StackedInline):
     model = Detail
-    extra = 1
 
 
 class BlogAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'tag_list', 'pub_time')
+    list_filter = ['author', 'tag']
     inlines = [DetailInline]
 
 
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['tag', 'blog_list']
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ['name', 'blog_list']
+
+
 admin.site.register(Blog, BlogAdmin)
+admin.site.register(Tag, TagAdmin)
+admin.site.register(Author, AuthorAdmin)
--- blog/models.py
+++ blog/models.py
@@ -3,23 +3,50 @@ from ckeditor_uploader.fields import RichTextUploadingField
 # Create your models here.
 
 
+class Author(models.Model):
+    name = models.CharField(max_length=50)
+    introduction = models.CharField(max_length=100, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def blog_list(self):
+        return ','.join([t.title for t in self.blog_set.all()])
+
+
+class Tag(models.Model):
+    tag = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.tag
+
+    def blog_list(self):
+        return ','.join([t.title for t in self.blog_set.all()])
+
+
 class Blog(models.Model):
     title = models.CharField(max_length=100)
-    category = models.CharField(max_length=50, blank=True)
+    tag = models.ManyToManyField(Tag)
     pub_time = models.DateTimeField(
         'date published', auto_now_add=True, editable=True)
-    author = models.CharField(max_length=50)
+    author = models.ForeignKey(Author)
     pined = models.BooleanField(default=False)
     summary = models.TextField(max_length=1000)
 
     def __str__(self):
         return self.title
 
+    def tag_list(self):
+        return ",".join([p.tag for p in self.tag.all()])
+
+    def get_detail(self):
+        return self.detail_set.all()
+
 
 class Detail(models.Model):
     blog = models.ForeignKey(Blog)
     content = RichTextUploadingField(null=True, blank=True)
-    capture = models.CharField(max_length=100)
+    capture = models.CharField(max_length=100, default='No.1')
     id = models.AutoField(primary_key=True)
 
     def __str__(self):
