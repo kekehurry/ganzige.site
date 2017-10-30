c5606a98b4ae2c54ac65730c70af95a7ce173c28
kekehurry <kekehurry@hotmail.com>
2017-10-30 22:47:16 +0800

    update

--- ganzige/views.py
+++ ganzige/views.py
@@ -6,8 +6,3 @@ def get_txt(request):
         text = f.read()
         f.close()
     return HttpResponse(text)
-
-
-def detail(request, page, image_id):
-    image = Photo.objects.get(id=image_id)
-    return render(request, 'photo/detail.html', {'image': image, 'page': page})
