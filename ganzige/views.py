1e6323145444ba68a6e4d365276e66954fc62822
kekehurry <kekehurry@hotmail.com>
2017-10-30 22:49:41 +0800

    update

--- ganzige/views.py
+++ ganzige/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import HttpResponse
 
 
 def get_txt(request):
-    with open(MP_verify_KdrNjdeEBujKWLaZ.txt, 'r', encoding='utf-8') as f:
+    with open('MP_verify_KdrNjdeEBujKWLaZ.txt', 'r', encoding='utf-8') as f:
         text = f.read()
         f.close()
     return HttpResponse(text)
