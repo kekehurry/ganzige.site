68a5b8e4beb5f360354b27751f1b1046e5416d9b
kekehurry <kekehurry@hotmail.com>
2017-10-31 08:21:57 +0800

    update

--- ganzige/urls.py
+++ ganzige/urls.py
@@ -17,7 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
-from . import views
 
 urlpatterns = [
     url(r'^$', include('home.urls', namespace='home')),
