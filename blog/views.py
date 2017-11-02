77e94576a1ad976445c994fd51886d68fa58d89e
kekehurry <kekehurry@hotmail.com>
2017-11-02 22:48:58 +0800

    commit

--- blog/views.py
+++ blog/views.py
@@ -15,7 +15,7 @@ def index(request, page=1):
     pined_blogs = Blog.objects.filter(pined=True).order_by('-pub_time')[:2]
 
     context = {'latest_blogs': latest_blogs,
-               'pined_blogs': pined_blogs, 'current_page': current_page,'tag_list':tag_list}
+               'pined_blogs': pined_blogs, 'current_page': current_page, 'tag_list': tag_list}
     return render(request, 'blog/index.html', context)
 
 
